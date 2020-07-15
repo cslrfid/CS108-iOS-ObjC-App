00ecc255e173c3c75493201795d45cad6df349ca
Carlson Lam <4546682+ksclam@users.noreply.github.com>
2020-07-15 12:09:06 -0400

    Issue with battery reporting
    - Fixed issue on battery reporting when battery level is in the range of 0-2%.

--- CS108iOSClient/model/CSLReaderBattery.m
+++ CS108iOSClient/model/CSLReaderBattery.m
@@ -92,7 +92,7 @@ static double _pcbVersion;
         _capacityTable=[NSArray arrayWithObjects:@100.0f, @99.0f, @98.0f, @97.0f, @96.0f, @94.0f, @92.0f, @89.0f, @85.0f, @80.0f, @75.0f, @70.0f, @65.0f, @60.0f, @55.0f, @50.0f, @45.0f, @40.0f, @35.0f, @30.0f, @24.0f, @20.0f, @16.0f, @13.0f, @9.0f, @6.0f, @2.0f, @0.0f, nil];
         
         _voltageSlope=[[NSMutableArray alloc] init];
-        for (cnt = 0; cnt < [_voltageTable count] - 2; cnt++) {
+        for (cnt = 0; cnt <= [_voltageTable count] - 2; cnt++) {
             [_voltageSlope addObject:@(([[_capacityTable objectAtIndex:cnt] doubleValue] - [[_capacityTable objectAtIndex:cnt + 1] doubleValue]) / ([[_voltageTable objectAtIndex:cnt] doubleValue] - [[_voltageTable objectAtIndex:cnt + 1] doubleValue]))];
         }
     }
@@ -102,7 +102,7 @@ static double _pcbVersion;
         _capacityTable=[NSArray arrayWithObjects:@100.0f, @99.0f, @98.0f, @97.0f, @95.0f, @93.0f, @90.0f, @87.0f, @84.0f, @81.0f, @78.0f, @75.0f, @73.0f, @71.0f, @69.0f, @68.0f, @66.0f, @64.0f, @62.0f, @60.0f, @58.0f, @56.0f, @54.0f, @52.0f, @50.0f, @48.0f, @47.0f, @45.0f, @43.0f, @41.0f, @39.0f, @37.0f, @35.0f, @33.0f, @31.0f, @29.0f, @27.0f, @26.0f, @24.0f, @22.0f, @20.0f, @18.0f, @16.0f, @14.0f, @12.0f, @10.0f, @8.0f, @6.0f, @5.0f, @3.0f, @1.0f, @0.0f, nil];
 
         _voltageSlope=[[NSMutableArray alloc] init];
-        for (cnt = 0; cnt < [_voltageTable count] - 2; cnt++) {
+        for (cnt = 0; cnt <= [_voltageTable count] - 2; cnt++) {
             [_voltageSlope addObject:@(([[_capacityTable objectAtIndex:cnt] doubleValue] - [[_capacityTable objectAtIndex:cnt + 1] doubleValue]) / ([[_voltageTable objectAtIndex:cnt] doubleValue] - [[_voltageTable objectAtIndex:cnt + 1] doubleValue]))];
         }
     }
